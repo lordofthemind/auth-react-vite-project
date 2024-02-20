734adbb613ba75f01708b0dfbc86b210bd2468d8
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-02-20 13:08:54 +0000

    transfered routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,33 @@ import Home from "./components/Home";
 import Register from "./components/Register";
 import Base from "./components/Base";
 import baseBackground from "./assets/FogInForest.jpg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard.jsx";
 
 function App() {
-  return <Base authComponent={<Login />} backgroundImage={baseBackground} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={
+            <Base authComponent={<Login />} backgroundImage={baseBackground} />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Base
+              authComponent={<Register />}
+              backgroundImage={baseBackground}
+            />
+          }
+        />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,34 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Base from "./components/Base";
-import baseBackground from "./assets/FogInForest.jpg";
-import Home from "./components/Home";
-import Login from "./components/Login";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Register from "./components/Register.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: (
-      <Base authComponent={<Login />} backgroundImage={baseBackground} />
-    ),
-  },
-  {
-    path: "/register",
-    element: (
-      <Base authComponent={<Register />} backgroundImage={baseBackground} />
-    ),
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );

d07cbe7ca544a8ad0d854326c72cd51bc0a097bc
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-02-22 12:27:18 +0000

    header working

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
-function Layout() {
+export default function Layout() {
   return (
-    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+    <div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-row">
       <Sidebar />
-      <div className="p-4">
-        <div className="bg-teal-100">Header</div>
+      <div className="flex flex-col flex-1">
+        <Header />
+        <div className="flex-1 p-4 min-h-0 overflow-auto">
+          <Outlet />
+        </div>
       </div>
-      <div className="bg-sky-100">{<Outlet />}</div>
     </div>
   );
 }
-
-export default Layout;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,41 +1,58 @@
-import React from "react";
-import { FaDAndD } from "react-icons/fa6";
 import {
   DASHBOARD_SIDEBAR_BOTTOM_LINKS,
   DASHBOARD_SIDEBAR_LINKS,
 } from "../lib/constant";
+import React from "react";
+import classNames from "classnames";
 import { Link, useLocation } from "react-router-dom";
+import { FcBullish } from "react-icons/fc";
+import { HiOutlineLogout } from "react-icons/hi";
 
-function Sidebar() {
+const linkClass =
+  "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
+
+export default function Sidebar() {
   return (
-    <div className="bg-neutral-900 w-60 p-3 flex flex-col text-white">
+    <div className="bg-neutral-900 w-60 p-3 flex flex-col">
       <div className="flex items-center gap-2 px-1 py-3">
-        <FaDAndD fontSize={24} />
-        <span className="text-neutral-100 text-lg">OpenShop</span>
+        <FcBullish fontSize={24} />
+        <span className="text-neutral-200 text-lg">OpenShop</span>
       </div>
-      <div className="flex-1">
+      <div className="py-8 flex flex-1 flex-col gap-0.5">
         {DASHBOARD_SIDEBAR_LINKS.map((link) => (
-          <SidebarLink key={link.key} item={link} />
+          <SidebarLink key={link.key} link={link} />
         ))}
       </div>
-      <div className="flex">
+      <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
         {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((link) => (
-          <SidebarLink key={link.key} item={link} />
+          <SidebarLink key={link.key} link={link} />
         ))}
+        <div className={classNames(linkClass, "cursor-pointer text-red-500")}>
+          <span className="text-xl">
+            <HiOutlineLogout />
+          </span>
+          Logout
+        </div>
       </div>
     </div>
   );
 }
 
-export default Sidebar;
-
-function SidebarLink({ item }) {
+function SidebarLink({ link }) {
   const { pathname } = useLocation();
 
   return (
-    <Link to={item.path} className="flex items-center gap-2 px-1 py-3">
-      {item.icon}
-      <span className="text-neutral-100">{item.label}</span>
+    <Link
+      to={link.path}
+      className={classNames(
+        pathname === link.path
+          ? "bg-neutral-700 text-white"
+          : "text-neutral-400",
+        linkClass
+      )}
+    >
+      <span className="text-xl">{link.icon}</span>
+      {link.label}
     </Link>
   );
 }
